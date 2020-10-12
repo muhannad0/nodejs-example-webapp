5f5299c3681e2023732f3085cd57ba1d699e98a1
Mohamed Muhannad <m.muhannad.h@gmail.com>
2020-10-12 15:40:23 +0500

    fix: connect to database

--- app.js
+++ app.js
@@ -4,28 +4,6 @@ require('dotenv').config()
 const port = process.env.SERVER_PORT || 3000
 const server_text = process.env.SERVER_TEXT || 'Hello World'
 
-const app = express()
-app.set('view engine', 'pug')
-app.set('views', __dirname)
-app.get('/', (req, res) => {
-  // res.send('Hello World')
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting: ' + err.stack)
-      //return
-      res.render('index', { heading: server_text, db_conn_status: 'FAIL' })
-    }
-
-    console.log('Database connected as: ' + connection.threadId)
-    res.render('index', { heading: server_text, db_conn_status: 'PASS' })
-  })
-  connection.end()
-})
-
-app.listen(port, () => {
-  console.log(`Web app listening at http://localhost:${port}`)
-})
-
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   host : process.env.DB_ADDRESS,
@@ -34,4 +12,26 @@ var connection = mysql.createConnection({
   user : process.env.DB_USERNAME,
   password : process.env.DB_PASSWORD
 })
+var db_conn_status = 'pending'
 
+connection.connect((err) => {
+  if (err) {
+    console.log('Error connecting to database: '+ err.code)
+    //console.log(err.fatal)
+    db_conn_status = 'FAIL'
+    return
+  }
+  console.log('Connected to database')
+  db_conn_status = 'PASS'	
+})
+
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', __dirname)
+app.get('/', (req, res) => {
+  res.render('index', { heading: server_text, db_conn_status: db_conn_status })
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
