fc5ca86142eae5d2e61efbb36db465b9cc9842c0
Mohamed Muhannad <m.muhannad.h@gmail.com>
2020-10-14 08:07:51 +0500

    fix: formatting

--- app.js
+++ app.js
@@ -18,29 +18,29 @@ var db_conn_msg_success = '<span style="color:green;">PASS</span>';
 function reconnect(){
   console.log('Connecting to database');
 
-	// Destroy existing connection since it cannot be re-used
+  // Destroy existing connection since it cannot be re-used
   //if (connection) connection.destroy();
-	connection = mysql.createConnection(db_config);
+  connection = mysql.createConnection(db_config);
 
   connection.connect((err) => {
     if (err) {
       console.log('Error connecting to database: ' + err.code);
       setTimeout(reconnect, 2000);
     } else {
-			console.log('Connected to database');
-			//return connection;
+      console.log('Connected to database');
+      //return connection;
     }
   });
 
-	// Handle server disconnect
-	connection.on('error', (err) => {
-		console.log('Error connecting to database: ' + err.code);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST'){
-			reconnect();
-		} else {
-			throw err;
-		}
-	});
+  // Handle server disconnect
+  connection.on('error', (err) => {
+    console.log('Error connecting to database: ' + err.code);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+      reconnect();
+    } else {
+      throw err;
+    }
+  });
 }
 
 // Attempt connection
@@ -50,7 +50,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname);
 app.get('/', (req, res) => {
-	var db_conn_status = (connection['state'] === 'connected' || connection['state'] === 'authenticated') ? db_conn_msg_success : db_conn_msg_fail;
+  var db_conn_status = (connection['state'] === 'connected' || connection['state'] === 'authenticated') ? db_conn_msg_success : db_conn_msg_fail;
   res.render('index', { heading: server_text, db_conn_status: db_conn_status });
 });
 
